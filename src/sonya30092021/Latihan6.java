5c80fd329da79d695821b24f63b8f51ecac46f61
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2022-09-30 20:56:16 +0700

    Create Latihan6.java

---
+++ src/sonya30092021/Latihan6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonya30092021;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+import javax.swing.JOptionPane;
+public class Latihan6 {
+    public static void main (String[] args){
+        String word1 = "";
+        String word2 = "";
+        String word3 = "";
+        
+        word1 = JOptionPane.showInputDialog("Enter word 1");
+        word2 = JOptionPane.showInputDialog("Enter word 2");
+        word3 = JOptionPane.showInputDialog("Enter word 3");
+        
+        String msg = word1 + " " + word2 + " " + word3; 
+
+        JOptionPane.showMessageDialog(null, msg); 
+    } 
+}
