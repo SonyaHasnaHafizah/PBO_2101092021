69eeb9eb2ff46560615f966184e6175d73372241
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2022-12-09 11:53:57 +0700

    Create Employee.java

---
+++ src/sonya091222/Employee.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sonya091222;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class Employee extends Person{
+    public String getName(){ 
+    System.out.println("Employee Name : " + name); 
+    return name; 
+    }  
+}
